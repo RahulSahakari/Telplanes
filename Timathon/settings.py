0716c31d097f6e0096309884101390ce206702d2
RahulSahakari <rajasahakari@gmail.com>
2021-06-30 17:39:43 +0530

    added static_dirs

--- Timathon/settings.py
+++ Timathon/settings.py
@@ -124,3 +124,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
